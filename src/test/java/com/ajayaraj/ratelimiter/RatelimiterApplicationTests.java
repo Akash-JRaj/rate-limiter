ed1b3a074f0716b127f3bb43ed8f1844fac6724b
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-14 12:14:49 +0530

    Initial commit

---
+++ src/test/java/com/ajayaraj/ratelimiter/RatelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajayaraj.ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatelimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
