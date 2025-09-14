d7e68b6f65f82379f30b61388211a5b92eaa3a58
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-14 17:03:15 +0530

    Implemented FixedWindowRateLimiter.

---
+++ src/main/java/com/ajayaraj/ratelimiter/service/FixedWindowRateLimiter.java
@@ -0,0 +1,35 @@
+package com.ajayaraj.ratelimiter.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Service
+public class FixedWindowRateLimiter implements RateLimiter {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    private final int LIMIT = 10;
+    private final int WINDOW_SIZE = 60;
+
+    @Override
+    public boolean isAllowed(String clientId) {
+
+        long windowStart = Instant.now().getEpochSecond() / WINDOW_SIZE;
+        String key = "rate_limit:" + clientId + ":" + windowStart;
+
+        System.out.println(key);
+
+        Long current = stringRedisTemplate.opsForValue().increment(key);
+
+        if(current == 1) {
+            stringRedisTemplate.expire(key, Duration.ofSeconds(WINDOW_SIZE));
+        }
+
+        return current <= LIMIT;
+    }
+}
