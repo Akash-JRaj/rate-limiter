007a8743b0750fbfaaa1fb582f59126d04b7ab3f
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-14 17:03:34 +0530

    Used FixedWindowRateLimiter in the controller.

--- src/main/java/com/ajayaraj/ratelimiter/controller/RateLimiterController.java
+++ src/main/java/com/ajayaraj/ratelimiter/controller/RateLimiterController.java
@@ -1,6 +1,8 @@
-package com.ajayaraj.ratelimiter.controllers;
+package com.ajayaraj.ratelimiter.controller;
 
 import com.ajayaraj.ratelimiter.models.Response;
+import com.ajayaraj.ratelimiter.service.FixedWindowRateLimiter;
+import com.ajayaraj.ratelimiter.service.RateLimiter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RateLimiterController {
 
+    private RateLimiter rateLimiter;
+
+    public RateLimiterController(FixedWindowRateLimiter fixedWindowRateLimiter) {
+        rateLimiter = fixedWindowRateLimiter;
+    }
+
     @GetMapping("/check")
     public ResponseEntity<Response> check(@RequestParam(required = true) String clientId) {
         Response response = new Response();
-        response.setAllowed(true);
+        response.setAllowed(rateLimiter.isAllowed(clientId));
         System.out.println(clientId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
