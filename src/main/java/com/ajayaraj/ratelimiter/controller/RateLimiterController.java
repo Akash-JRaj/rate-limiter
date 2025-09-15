175ae81552beec1eb5fdb723b351dabadfe61f5e
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-15 20:55:32 +0530

    Modifications to RateLimiterController

--- src/main/java/com/ajayaraj/ratelimiter/controller/RateLimiterController.java
+++ src/main/java/com/ajayaraj/ratelimiter/controller/RateLimiterController.java
@@ -3,6 +3,8 @@ package com.ajayaraj.ratelimiter.controller;
 import com.ajayaraj.ratelimiter.models.Response;
 import com.ajayaraj.ratelimiter.service.FixedWindowRateLimiter;
 import com.ajayaraj.ratelimiter.service.RateLimiter;
+import com.ajayaraj.ratelimiter.service.SlidingWindowCounterRateLimiter;
+import com.ajayaraj.ratelimiter.service.SlidingWindowLogRateLimiter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +16,14 @@ public class RateLimiterController {
 
     private RateLimiter rateLimiter;
 
-    public RateLimiterController(FixedWindowRateLimiter fixedWindowRateLimiter) {
-        rateLimiter = fixedWindowRateLimiter;
+    public RateLimiterController(SlidingWindowCounterRateLimiter rateLimiter) {
+        this.rateLimiter = rateLimiter;
     }
 
     @GetMapping("/check")
     public ResponseEntity<Response> check(@RequestParam(required = true) String clientId) {
         Response response = new Response();
         response.setAllowed(rateLimiter.isAllowed(clientId));
-        System.out.println(clientId);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
