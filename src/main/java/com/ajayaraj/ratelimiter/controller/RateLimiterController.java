561cb5ce5b5b7add314ecabaef036ceaff19cad4
Akash Jayaraj <akashjayarajvkp2k02@gmail.com>
2025-09-16 21:57:19 +0530

    Modified RateLimiterController to use RateLimiterFactory.

--- src/main/java/com/ajayaraj/ratelimiter/controller/RateLimiterController.java
+++ src/main/java/com/ajayaraj/ratelimiter/controller/RateLimiterController.java
@@ -1,10 +1,12 @@
 package com.ajayaraj.ratelimiter.controller;
 
+import com.ajayaraj.ratelimiter.factory.RateLimiterFactory;
 import com.ajayaraj.ratelimiter.models.Response;
 import com.ajayaraj.ratelimiter.service.FixedWindowRateLimiter;
 import com.ajayaraj.ratelimiter.service.RateLimiter;
 import com.ajayaraj.ratelimiter.service.SlidingWindowCounterRateLimiter;
 import com.ajayaraj.ratelimiter.service.SlidingWindowLogRateLimiter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RateLimiterController {
 
-    private RateLimiter rateLimiter;
-
-    public RateLimiterController(SlidingWindowCounterRateLimiter rateLimiter) {
-        this.rateLimiter = rateLimiter;
-    }
+    @Autowired
+    private RateLimiterFactory rateLimiterFactory;
 
     @GetMapping("/check")
     public ResponseEntity<Response> check(@RequestParam(required = true) String clientId) {
         Response response = new Response();
-        response.setAllowed(rateLimiter.isAllowed(clientId));
+        response.setAllowed(rateLimiterFactory.getRateLimiter().isAllowed(clientId));
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
